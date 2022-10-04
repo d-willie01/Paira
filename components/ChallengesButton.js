2189b7dd2eef7abb07a040396cc395f998c52849
twill32 <99900933+twill32@users.noreply.github.com>
2022-10-04 15:51:59 -0700

    Update ChallengesButton.js

--- components/ChallengesButton.js
+++ components/ChallengesButton.js
@@ -3,7 +3,7 @@ import styles from "../Styles/styles";
 import { TouchableOpacity, SafeAreaView, Text} from "react-native";
 import { EvilIcons } from "@expo/vector-icons";
 
-const ChallengesButton = ({text}) =>{
+const ChallengesButton = () =>{
     return(
         <TouchableOpacity style={styles.button}>
             <EvilIcons name = "star" size= {50} color = "#D7D7D7"/>
