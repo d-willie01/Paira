dc3a9a43a0e3521ca3ba8fd5f5a6301e06265298
d-willie01 <dariusw0325@gmail.com>
2022-10-21 02:43:18 -0700

    Maps Created

--- Screens/MainSearchScreen.js
+++ Screens/MainSearchScreen.js
@@ -1,10 +1,27 @@
 import React from "react";
-import {Text} from 'react-native'
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 const UserSearchMainScreen = () => {
     return(
-        <Text>Hello World</Text>
+        <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
     );
 };
 
 export default UserSearchMainScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
