04bd1377e1fbbf7438cda3e525482e1a45546357
d-willie01 <dariusw0325@gmail.com>
2022-10-20 17:41:18 -0700

    Little change to business side

--- BNavigation/BHomeStack.js
+++ BNavigation/BHomeStack.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BUserHomeScreen from "../BScreens/BUserHomeScreen";
 import BusinessHomeScreen from "../Screens/BusinessHomeScreen";
+import BusinessHomeScreenTest from "../Screens/BusinessHomeScreenTest";
 
 
 const BNav = createNativeStackNavigator();
@@ -10,7 +11,7 @@ const BHomeNavigationStack = () => {
     return(
         
             <BNav.Navigator initialRouteName='BHomeScreen' >
-                <BNav.Screen options={{headerShown: false}} name = 'BHomeScreen' component={BusinessHomeScreen}/>
+                <BNav.Screen options={{headerShown: false}} name = 'BHomeScreen' component={BusinessHomeScreenTest}/>
             </BNav.Navigator>
         
     );
