e2d190dae9aee627eb98363c66839b60d0dac43b
twill32 <99900933+twill32@users.noreply.github.com>
2022-10-26 22:42:18 -0500

    Update UserRegistrationScreen.js
    Created protected password with Icon

--- RegistrationScreens/UserRegistrationScreen.js
+++ RegistrationScreens/UserRegistrationScreen.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import {Text, View, Button, Image, TouchableOpacity, TextInput} from 'react-native'
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import styles from "../Styles/styles";
+import { Feather } from '@expo/vector-icons'; 
+
 
 const UserRegistrationScreen = () => {
     const Startnavigation = useNavigation();
@@ -10,6 +12,8 @@ const UserRegistrationScreen = () => {
    const UserRegister = () => {
         Startnavigation.navigate("Registration")
    }
+   const [isSecureEntry, setIsSecureEntry] = useState(true);
+   const [text, onChangeText] = useState('');
 
     return(
         <View style = {{
@@ -27,7 +31,7 @@ const UserRegistrationScreen = () => {
             <LinearGradient
                     style={{
                         height: 45,
-                        width: 250,
+                        width: 290,
                         borderRadius: 75,
                         alignItems:'center',
                         marginBottom: 20,
@@ -39,7 +43,7 @@ const UserRegistrationScreen = () => {
                     <LinearGradient
                         style={{
                             height: 40,
-                            width: 245,
+                            width: 285,
                             borderRadius: 75,
                             alignItems:'center',
                             }}
@@ -49,7 +53,7 @@ const UserRegistrationScreen = () => {
                                             fontSize: 25,
                                             height: 55,
                                             width: 300,
-                                            marginLeft: 95,
+                                            marginLeft: 50,
                                             marginTop: -5,
                                             borderRadius: 75,
                                             fontWeight: "900",
@@ -61,6 +65,57 @@ const UserRegistrationScreen = () => {
                 </LinearGradient>
                 </TouchableOpacity>
                 <TouchableOpacity >
+                <LinearGradient
+                    style={{
+                        height: 45,
+                        width: 290,
+                        borderRadius: 75,
+                        alignItems:'center',
+                        marginBottom: 20,
+                        marginLeft: 10,
+                        bottom: 5
+                    }}
+                    colors={['#F8C471','#FF8900' ]}>
+                        
+                    <LinearGradient
+                        style={{
+                            height: 40,
+                            width: 285,
+                            borderRadius: 75,
+                            
+                            }}
+                        colors={['#F8C471','#F5B041' ]}>
+                            <TextInput style={{
+                                            fontSize: 25,
+                                            height: 55,
+                                            width: 260,
+                                            
+                                            marginLeft: 20,
+                                            marginTop: -5,
+                                            borderRadius: 75,
+                                            fontWeight: "900",
+                                            color: '#ffffff'}}
+                                            placeholder = "Enter Password"
+                                            secureTextEntry={isSecureEntry}
+                                            onChangeText={(value) => {
+                                                onChangeText({name: 'password', value});
+                                            }}
+                                            >
+                            </TextInput>
+                        </LinearGradient>
+                </LinearGradient>
+            </TouchableOpacity>
+            <View style={{position:'absolute', marginTop: 73, right:17,}}>
+                <TouchableOpacity
+
+                    onPress={()=>{
+                        setIsSecureEntry((prev)=> !prev)
+
+                                }}>
+                                <Feather name = {isSecureEntry ? 'eye-off':'eye'} size={20} color="black"/>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity title = "Next screen" onPress={UserRegister}>
                 <LinearGradient
                     style={{
                         height: 45,
@@ -81,21 +136,21 @@ const UserRegistrationScreen = () => {
                             alignItems:'center',
                             }}
                         colors={['#F8C471','#F5B041' ]}>
-                            <TextInput style={{
+                            <Text style={{
                                             fontSize: 25,
                                             height: 55,
                                             width: 300,
-                                            marginLeft: 95,
-                                            marginTop: -5,
+                                            marginLeft: 90,
+                                            marginTop: 5,
                                             borderRadius: 75,
+                                            
                                             fontWeight: "900",
-                                            color: '#ffffff'}}
-                                            placeholder = "Password">
-                            </TextInput>
+                                            color: 'white'}}
+                                            >Get Registered
+                            </Text>
                         </LinearGradient>
                 </LinearGradient>
             </TouchableOpacity>
-            <Button title = "Next screen" onPress={UserRegister}/>
         </View>
         </LinearGradient>
         
@@ -104,3 +159,4 @@ const UserRegistrationScreen = () => {
 };
 
 export default UserRegistrationScreen;
+
