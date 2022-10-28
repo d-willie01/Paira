f1f9ad658d8e4360073cf4834eda9ddaca2ab037
d-willie01 <dariusw0325@gmail.com>
2022-10-28 15:25:59 -0700

    Data Updates

--- assets/Data/PlacesData.js
+++ assets/Data/PlacesData.js
@@ -1,30 +1,30 @@
 const DATA = [{
     id: 3,
     image:"https://i0.wp.com/www.therepublicofrose.com/wp-content/uploads/2021/04/DSC05060-scaled.jpg?fit=3936%2C2624&ssl=1",
-    founded: "Founded in 1997",
-    title: "Scott's Corner!",
-    bio:"Right in the heart of Scottsdale you can stay eat drink laugh love in this amazing corner of Arizona!",
-    address: "1567 E Rural Rd", 
-    link: "www.ScottsAlotts.com",
+    founded: "Founded in 1900",
+    title: "Sedona",
+    bio:"A staple Arizona, attraction that  must be visited if you are a tourist!",
+    address: "1000 N Sedona Rd", 
+    link: "www.Sedona.gov/EPA",
     
 },
 {
     id: 4,
     image:"https://drivinvibin.com/wp-content/uploads/2021/04/marina-at-saguaro-lake_t20_BmlwKZ.jpeg",
-    founded: "Founded in 2018",
-    title: "Hifi",
-    bio:"One stop all for entertainment. Music. Drinks. People. Life. Here at Hifi!",
-    address: "1122 OldTown Rd.",
-    link: "https://letsgetRight.org"
+    founded: "Founded in 1900",
+    title: "Saguaro Lake",
+    bio:"Big beautiful boats! Awesome places and crazy times!",
+    address: "E Saguaro Lake",
+    link: "https://SaguaroLake.org"
 },
 {
     id: 5,
     image:"https://www.mygrandcanyonpark.com/wp-content/uploads/2019/10/Skywalk_CaitlinMadisonKirkman_LeibowitzSolo_GCW_2400.jpg",
-    founded: "Founded in 2020",
-    title: "Downtown Gallery",
-    bio:"Architecture in the form of art available for all to see at the ASU Downtown Campus!",
-    address: "222 E 5th Ave",
-    link: "www.ArtForUs.com"
+    founded: "1900",
+    title: "Grand Canyon Lodge",
+    bio:"wonder of the World! Stay here for cheap and be on your way to hike and roma over the fast Grand Canyon",
+    address: "2000 GrandCanyon Blvd",
+    link: "www.GrandCanyon.com"
 }
 ]
 
