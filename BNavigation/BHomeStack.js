afd59db02a1aa9d8b00ecce1ba4f8e2ac46a9d05
twill32 <99900933+twill32@users.noreply.github.com>
2022-10-30 21:14:22 -0500

    Fixed Navigation
    Switched Navigation branch to correct Business Homescreen

--- BNavigation/BHomeStack.js
+++ BNavigation/BHomeStack.js
@@ -11,8 +11,8 @@ const BHomeNavigationStack = () => {
     return(
         
             <BNav.Navigator initialRouteName='BHomeScreen' >
-                <BNav.Screen options={{headerShown: false}} name = 'BHomeScreen' component={BusinessHomeScreenTest}/>
-                <BNav.Screen options={{headerShown: false}} name= 'Organization Keywords' component ={BusinessAddKeyWordsScreen}/>
+                <BNav.Screen options={{headerShown: false}} name = 'BHomeScreen' component={BUserHomeScreen}/>
+                
             </BNav.Navigator>
         
     );
