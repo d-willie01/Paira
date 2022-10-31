8f5bbefa68efd1ac70cf1245ba423f337a757d81
twill32 <99900933+twill32@users.noreply.github.com>
2022-10-31 01:53:29 -0500

    percent changes
    percent changes

--- RegistrationScreens/LoginScreen.js
+++ RegistrationScreens/LoginScreen.js
@@ -2,100 +2,64 @@ import React from "react";
 import {Text, View, TouchableOpacity, TextInput} from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
 import styles from "../Styles/styles";
+import { RegistrationFills, PasswordFills, EyeButton } from "./UserRegistrationScreenFills";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp, heightPercentageToDP, widthPercentageToDP} from "react-native-responsive-screen";
+import { LoginButton } from "./RegistrationButtons";
 
 
 const LoginScreen = () => {
     return(
-        <View style = {{
-            flex:1,
-             }}>
+        <View 
+        style={{
+            height: heightPercentageToDP('100%'),
+            width: widthPercentageToDP('100%'),
+            }}>
             <LinearGradient // background gradient view
                 style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
                 
                 colors={['#FDFEFE','#626567']}>
-            <View style={{marginBottom: 50}}>
+            <View style={{alignItems:'center', width:'100%', height:'10%', backgroundColor:'transparent'}}>
             <Text style={{fontWeight:'900', fontSize: 45, color: 'white'}}>Welcome Back! </Text>
             </View>
-            <View style={{alignItems: 'center'}}>
+            <View style={{
+                height:'8%',
+                 width: '100%',
+                 backgroundColor:'transparent',
+                 alignItems:'center', 
+                 alignItems: 'center', 
+                 marginTop: '15%',
+                 }}>
+            <RegistrationFills placeholder = "Email"/>
             
-            <TouchableOpacity style={{alignItems:'center',justifyContent: 'center'}}>
-            <LinearGradient
-                    style={{
-                        height: 45,
-                        width: 250,
-                        borderRadius: 75,
-                        alignItems:'center',
-                        marginBottom: 20,
-                        marginLeft: 10,
-                        bottom: 5
-                    }}
-                    colors={['#F8C471','#FF8900' ]}>
-                        
-                    <LinearGradient
-                        style={{
-                            height: 40,
-                            width: 245,
-                            borderRadius: 75,
-                            alignItems:'center',
-                            }}
-                        colors={['#F8C471','#F5B041' ]}>
-                            <View >
-                            <TextInput style={{
-                                            fontSize: 20,
-                                            height: 55,
-                                            width: 300,
-                                            marginLeft: 95,
-                                            marginTop: -5,
-                                            borderRadius: 75,
-                                            fontWeight: "900",
-                                            opacity: 0.7,
-                                            color: '#ffffff'}}
-                                            placeholderTextColor="#E5E5E5"
-                                            placeholder = "Email/Phone Number">
-                            </TextInput>
-                            </View>
-                        </LinearGradient>
-                </LinearGradient>
-                </TouchableOpacity>
-                <TouchableOpacity >
-                <LinearGradient
-                    style={{
-                        height: 45,
-                        width: 250,
-                        borderRadius: 75,
-                        alignItems:'center',
-                        marginBottom: 20,
-                        marginLeft: 10,
-                        bottom: 5
-                    }}
-                    colors={['#F8C471','#FF8900' ]}>
-                        
-                        <LinearGradient
-                            style={{
-                                height: 40,
-                                width: 245,
-                                borderRadius: 75,
-                                alignItems:'center',
-                                }}
-                            colors={['#F8C471','#F5B041' ]}>
-                                <TextInput style={{
-                                                fontSize: 23,
-                                                height: 55,
-                                                width: 300,
-                                                marginLeft: 95,
-                                                marginTop: -5,
-                                                borderRadius: 75,
-                                                fontWeight: "900",
-                                                opacity: .7,
-                                                color: '#ffffff'}}
-                                                placeholderTextColor="#E5E5E5"
-                                                placeholder = "Password">
-                                </TextInput>
-                        </LinearGradient>
-                </LinearGradient>
-            </TouchableOpacity>
-        </View>
-        
+             </View>
+                       
+                
+                <View style={{height:'5%',
+                 width: '100%',
+                 backgroundColor:'transparent',
+                 alignItems:'center', 
+                 alignItems: 'center', 
+                 }}>
+            <PasswordFills placeholder = "Password"/>
+                <View style={{position:'absolute',  right:'20%',marginTop:'1.5%'}}>
+                    <EyeButton/>
+                </View>
+            </View>
+            <View 
+            style={{
+            height: '15%',
+            width: '100%' , 
+            alignItems: 'center', 
+            backgroundColor:'transparent',}}>
+            <TouchableOpacity title ="Login" onPress={LoginScreen} 
+                style={{
+                    height: '65%',
+                    width: '55%' , 
+                    marginTop:'5%',
+                    }}>
+                <LoginButton text ='Login'/>
+            </TouchableOpacity> 
+            </View>      
         </LinearGradient>
         </View>
     );
